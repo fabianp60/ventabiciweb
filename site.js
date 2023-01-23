357148e907bb129c3d8c9dad85133db4a44968f1
fabian.pena <fabian.pena@cumplimos.co>
2023-01-23 14:38:06 -0500

    Ajustes de bicicleta vendida

--- site.js
+++ site.js
@@ -1,9 +1,14 @@
 const offerInput = document.getElementById("myoffer-input"); 
 const offerLink = document.getElementById("wa-offer-button");
-const baseLinkText = "https://wa.me/573006797466?text=Hola%2C%20quiero%20ofertar%20por%20la%20bicicleta%20%24";
+const baseLinkText = "https://wa.me/570000000000?text=Hola%2C%20quiero%20ofertar%20por%20la%20bicicleta%20%24";
 
 function updateOfferLink() {
     offerLink.href = `${baseLinkText}${offerInput.value}`;
 }
+
+offerLink.addEventListener('click', (evt) => {
+    alert("¡Lo siento, la bicicleta ya fué vendida!");
+    evt.preventDefault();
+});
 
 offerInput.onchange = updateOfferLink;
